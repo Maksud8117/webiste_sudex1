a3c54e4917e0ab2933aff5bf667b857e7b2af8cf
Maksud8117 <61826793+Maksud8117@users.noreply.github.com>
2021-06-28 16:28:07 +0500

    Add files via upload

---
+++ src/test/java/com/website/sudex1/Sudex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.sudex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sudex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
